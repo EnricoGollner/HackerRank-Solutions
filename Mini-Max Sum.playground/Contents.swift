5a31ca882755e47cbbcf73a1c7f6a34aa2fbf0d4
EnricoGollner <enricogollner.tech@gmail.com>
2023-05-23 02:16:59 -0300

    minMaxSum

--- Mini-Max Sum.playground/Contents.swift
+++ Mini-Max Sum.playground/Contents.swift
@@ -6,18 +6,29 @@ import Cocoa
  Given five positive integers, find the minimum and maximum values that can be calculated by summing exactly four of the five integers. Then print the respective minimum and maximum values as a single line of two space-separated long integers.
  */
 
-let arr = [1, 2, 3, 4, 5]
-
-//let arr = [5, 5, 5, 5, 5]
+let arr = [1, 3, 5, 7, 9]  // 16 24
 
 func miniMaxSum(arr: [Int]) -> Void {
-    arr.sorted()
-    let range = arr.count
+    var minNum = arr[0]
+    var maxNum = arr[0]
+    var sum = 0
+
+    for i in 0..<arr.count {
+        
+        if arr[i] < minNum {  // numero atual é maior do que o inicial...
+            minNum = arr[i]
+        }
+        
+        if arr[i] > maxNum {
+            maxNum = arr[i]
+        }
+        sum += arr[i]
+    }
     
-    var minSum = 0
-    var maxSum = 0
+    let minSum = sum - maxNum  // A miníma soma é a soma de todos os num - o número máx
+    let maxSum = sum - minNum
     
-    
+    print("\(minSum) \(maxSum)")
 }
 
 miniMaxSum(arr: arr)
