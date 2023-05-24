4a32537bc34fb8a11be60361747dbe5b12174226
EnricoGollner <enricogollner.tech@gmail.com>
2023-05-24 02:40:10 -0300

    Grading Students challange

---
+++ Warmup challenges in Algorithms/Grading Students.playground/Contents.swift
@@ -0,0 +1,40 @@
+import Cocoa
+
+/*
+
+ HackerLand University has the following grading policy:
+
+ Every student receives a  in the inclusive range from  0 to 100.
+ Any grade less than 40 is a failing grade.
+ Sam is a professor at the university and likes to round each student's grade according to these rules:
+ 
+ If the difference between the grade and the next multiple of 5 is less than 3 (módulo - %), round grade up to the next multiple of 5.
+ If the value of grade is less than 38, no rounding occurs as the result will still be a failing grade.
+ 
+ Returns:
+ int[n]: the grades after rounding as appropriate
+ 
+*/
+
+let grades = [4, 73, 67, 38, 33]
+
+func gradingStudents(grades: [Int]) -> [Int] {
+    
+    var finalGrades = [Int]()
+    
+    for i in 0..<grades.count {
+        let nextMultiple = (grades[i] / 5 + 1) * 5
+        let diff = nextMultiple - grades[i]
+        
+        if diff < 3 && grades[i] >= 38 {
+            finalGrades.append(grades[i] + diff)
+        }
+        else {
+            finalGrades.append(grades[i])
+        }
+    }
+    
+    return finalGrades
+}
+
+print(gradingStudents(grades: grades))
