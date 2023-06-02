21a7658a5379e191b6d4ef24a840743a518d9104
EnricoGollner <enricogollner.tech@gmail.com>
2023-06-02 01:56:05 -0300

    Day10

---
+++ 30DaysOfCode/Day10.playground/Contents.swift
@@ -0,0 +1,32 @@
+
+import Foundation
+
+/*
+ Day 10: Binary Numbers
+ 
+ Print a single base-10 integer that denotes the maximum number of consecutive 1's in the binary representation of n.
+ 
+*/
+
+var num = 13
+var sum = 0
+var max = 0
+
+while num > 0 {
+    if num % 2 == 1 {
+        sum += 1
+
+        if sum > max {  // se soma == 1
+            max = sum
+        }
+
+    } else {
+        sum = 0
+    }
+
+    num = num / 2
+}
+
+print(max)
+
+
